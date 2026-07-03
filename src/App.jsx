612a7bc4ea8dc565e5ea019090909122eabaa9dc
Asim Ali <asimaliii1121@gmail.com>
2026-07-03 18:16:33 +0500

    Update header and make private route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes , Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -6,19 +6,22 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
 import Header from './components/Header'
-
+import PrivateRoute from './components/PrivateRoute'
 function App() {
-
-  return <BrowserRouter>
-  <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/signin' element={<SignIn/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-
+import { useSelector } from 'react-redux'
+import { current } from '@reduxjs/toolkit'
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user)
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -30,10 +32,19 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="/signin">
-            <li className="hover:text-blue-400 cursor-pointer text-blue-400">
-              Sign In
-            </li>
+
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.photo}
+                alt="profile"
+                className="w-8 h-8 rounded-full"
+              />
+            ) : (
+              <li className="hover:text-blue-400 cursor-pointer text-blue-400">
+                Sign In
+              </li>
+            )}
           </Link>
         </ul>
       </div>
